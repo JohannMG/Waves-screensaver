05567835bea8b014c5c6897e28e3102cedf94745
Johann Garces <hello@johannmg.com>
2017-10-28 21:10:18 -0700

    Initial Commit

---
+++ Wave1/Wave1View.h
@@ -0,0 +1,13 @@
+//
+//  Wave1View.h
+//  Wave1
+//
+//  Created by Johann Garces on 10/28/17.
+//  Copyright © 2017 johannmg. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Wave1View : ScreenSaverView
+
+@end
---
+++ Wave1/Wave1View.m
@@ -0,0 +1,52 @@
+//
+//  Wave1View.m
+//  Wave1
+//
+//  Created by Johann Garces on 10/28/17.
+//  Copyright © 2017 johannmg. All rights reserved.
+//
+
+#import "Wave1View.h"
+
+@implementation Wave1View
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
